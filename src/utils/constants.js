e44cf1468f71f03d077f31b2287042ecbb1d7ca5
dennisja <dennisjjagwe@gmail.com>
2018-09-03 19:18:42 +0300

    Update constants comments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 // TODO: fetch url from an environment variable
 export const API_URL = 'http://localhost:4466';
 
+// TODO: fetch application local storage key in environment variables as well
 export const AUTH_TOKEN_KEY = 'QBUZZ_AUTH_TOKEN';
